ab984cbc8753d1ae92d16a8b90ffed59cafa5c23
PaperPlate <106558970+ThePaperPlate@users.noreply.github.com>
2024-08-29 20:10:58 -0700

    Mark dirty and update outputs immediately
    Resolves an issue where IO entities saved in an "on" state required
    immediate power to maintain that state.
    
    Always refresh industrial prevent building

--- CopyPaste.cs
+++ CopyPaste.cs
@@ -2200,8 +2200,10 @@ namespace Oxide.Plugins
                 }
             }
 
-            ioEntity.MarkDirtyForceUpdateOutputs();
+            ioEntity.MarkDirty();
+            ioEntity.UpdateOutputs();
             ioEntity.SendNetworkUpdate();
+            ioEntity.RefreshIndustrialPreventBuilding();
         }
 
         private void SetItemSubEntity(PasteData pasteData, Item item, ulong oldId)
